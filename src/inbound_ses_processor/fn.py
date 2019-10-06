bcda38811b6bc63a52ae4355d7db3e1ddf895dda
Brad Campbell <bsc3j@alumni.virginia.edu>
2019-10-05 23:36:16 -0400

    [feat] whitelisted senders, comments, try/catch logic

--- src/inbound_ses_processor/fn.py
+++ src/inbound_ses_processor/fn.py
@@ -8,11 +8,19 @@ MAIL_BUCKET = os.getenv("mail_bucket_name", "")
 MAIL_BUCKET_REGION = os.getenv("mail_bucket_region", os.environ["AWS_DEFAULT_REGION"])
 MAIL_DOMAIN = os.getenv("mail_domain", "")
 LOG_LEVEL = os.getenv("log_level", "INFO")
+ACCEPT_FROM = os.getenv("accept_from", "")
 
 logger = logging.getLogger()
 
 
 def parse_email_content(msg, s3_handle, message_id):
+    """If this is a multipart message, recursively iterate through each
+    part, extracting all attachments and uploading each to a message-specific
+    prefix within an attachments prefix.
+
+    Based on example from:
+        https://www.dev2qa.com/python-parse-emails-and-attachments-from-pop3-server-example/
+    """
     # get message content type.
     content_type = msg.get_content_type().lower()
     logger.info(content_type)
@@ -39,41 +47,88 @@ def parse_email_content(msg, s3_handle, message_id):
         ]
 
         # get attached file content.
-        attach_file_data = msg.get_payload(decode=True)
-        s3_handle.put_object(
-            Body=attach_file_data,
-            Bucket=MAIL_BUCKET,
-            Key=f"@attachments/{message_id}/{attach_file_name}",
-        )
+        try:
+            attach_file_data = msg.get_payload(decode=True)
+            s3_handle.put_object(
+                Body=attach_file_data,
+                Bucket=MAIL_BUCKET,
+                Key=f"@attachments/{message_id}/{attach_file_name}",
+            )
+        except Exception as e:
+            logger.error(
+                {
+                    f"Failed to extract and upload to s3 '{message_id}/{attach_file_name}'": f"{repr(e)}"
+                }
+            )
+            raise e
 
     else:
         pass
 
 
 def handler(event, context):
+    """Main event handler.  Sets up logging, parses relevent info from SES
+    event notification, shuffles message object to relevant prefixes, and
+    attempts to process attachment(s) in inbound messages."""
+    # Set up logging
     aws_lambda_logging.setup(level=LOG_LEVEL, boto_level="CRITICAL")
-    s3 = boto3.client("s3", region_name=MAIL_BUCKET_REGION)
     logger.debug(event)
+    # Set up s3 client
+    s3 = boto3.client("s3", region_name=MAIL_BUCKET_REGION)
 
+    # Extract relevant values from the record and log them
     ses_notification = event["Records"][0]["ses"]
     message_id = ses_notification["mail"]["messageId"]
+    mail_from = ses_notification["mail"]["source"]
     receipt = ses_notification["receipt"]
-    logger.info({"message_id": f"{message_id}"})
-    logger.info({"receipt": f"{receipt}"})
+    accepted_senders = ACCEPT_FROM.split("|")
+    logger.info({"message_id": message_id})
+    logger.info({"receipt": receipt})
+    logger.info({"mail_from": mail_from})
 
-    num_recipients = len(receipt["recipients"])
-    recipient_prefixes = [x.split("@")[0] for x in receipt["recipients"]]
-    copy_source = {"Bucket": MAIL_BUCKET, "Key": message_id}
-    for recipient_prefix in recipient_prefixes:
-        s3.copy_object(
-            CopySource=copy_source,
-            Bucket=MAIL_BUCKET,
-            Key=f"{recipient_prefix}/cur/{message_id}",
+    if mail_from in accepted_senders or accepted_senders == [""]:
+        logger.info("mail_from in accepted_senders")
+        # Parse recipient(s) so that we can place the message in recipient-specific prefixes
+        num_recipients = len(receipt["recipients"])
+        recipient_prefixes = [x.split("@")[0] for x in receipt["recipients"]]
+        copy_source = {"Bucket": MAIL_BUCKET, "Key": message_id}
+        # Loop through parsed recipients and copy message to each prefix
+        for recipient_prefix in recipient_prefixes:
+            try:
+                s3.copy_object(
+                    CopySource=copy_source,
+                    Bucket=MAIL_BUCKET,
+                    Key=f"{recipient_prefix}/cur/{message_id}",
+                )
+            except Exception as e:
+                logger.error(
+                    {
+                        f"Exception caught trying to copy '{message_id}' to '{recipient_prefix}/cur/{message_id}'": f"{repr(e)}"
+                    }
+                )
+
+        # Attempt to download the message locally and parse its contents
+        try:
+            data = s3.get_object(Bucket=MAIL_BUCKET, Key=f"{message_id}")
+            contents_decoded = data["Body"].read().decode("utf-8")
+            msg = Parser().parsestr(contents_decoded)
+            parse_email_content(msg, s3, message_id)
+        except Exception as e:
+            logger.error(
+                {
+                    f"Exception caught trying to parse message '{message_id}'": f"{repr(e)}"
+                }
+            )
+            raise e
+    else:
+        logger.info("mail_from not in accepted_senders")
+
+    # In either case, we want to delete the inbound object
+    try:
+        s3.delete_object(Bucket=MAIL_BUCKET, Key=message_id)
+        logger.info({"Object deleted": f"{message_id}"})
+    except Exception as e:
+        logger.error(
+            {f"Exception caught trying to delete_object '{message_id}'": f"{repr(e)}"}
         )
-
-    data = s3.get_object(Bucket=MAIL_BUCKET, Key=f"{message_id}")
-    contents_decoded = data["Body"].read().decode('utf-8')
-    msg = Parser().parsestr(contents_decoded)
-    parse_email_content(msg, s3, message_id)
-    
-    s3.delete_object(Bucket=MAIL_BUCKET, Key=message_id)
+        raise e
